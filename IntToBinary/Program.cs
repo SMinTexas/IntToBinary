0a2e0149588b0a6bfd9881aafb1d5e41a088e974
Steven Murray <32107837+SMinTexas@users.noreply.github.com>
2019-03-26 20:31:51 -0500

    Add files via upload

---
+++ IntToBinary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Integer_to_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            prog pg = new prog();
+            Console.WriteLine("Enter a number : ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The binary equivalent of num {0} is ", num);
+            pg.binaryConversion(num);
+            Console.Read();
+        }
+    }
+    public class prog
+    {
+        public int binaryConversion(int num)
+        {
+            int bin;
+            if (num != 0)
+            {
+                bin = (num % 2) + 10 * binaryConversion(num / 2);
+                Console.Write(bin);
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
